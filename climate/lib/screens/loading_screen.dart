c11aa59f9ea5e427f0ecb4f76d7f003086fe135b
ASUS <minhthoai190892@gmail.com>
2023-09-27 13:35:39 +0700

    145.-Application-Programming-Interfaces-(APIs)-Explained

--- climate/lib/screens/loading_screen.dart
+++ climate/lib/screens/loading_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:climate/services/album.dart';
 import 'package:climate/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -14,8 +18,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -23,6 +27,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http
+        .get(Uri.parse('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e21c0a31b1307e9263b6baee3c15eb88'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
