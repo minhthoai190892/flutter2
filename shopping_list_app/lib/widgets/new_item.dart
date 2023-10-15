f2ec683a6c6a6acb7d4d47d831563c87eb797045
minhthoai <minhthoai190892@gmail.com>
2023-10-15 15:44:53 +0700

    add textformfile

---
+++ shopping_list_app/lib/widgets/new_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+            child: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+              ),
+              validator: (value) {
+                return 'Demo...';
+              },
+            ),
+            Row(
+              children: [
+                TextFormField()
+              ],
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
