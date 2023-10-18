a3c5bdaa3ef99906449d87499219df908decd9b5
minhthoai <minhthoai190892@gmail.com>
2023-10-18 11:15:04 +0700

    place_app

---
+++ favorite_places/lib/model/place.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Place {
+  final String id;
+  final String title;
+  Place({
+    required this.id,
+    required this.title,
+  });
+}
