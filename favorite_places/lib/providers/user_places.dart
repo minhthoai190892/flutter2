f930c6dcebf9346a5c7fad093ecbf23dc39f4947
minhthoai <minhthoai190892@gmail.com>
2023-10-21 07:39:43 +0700

    Storing the Picked Image Locally

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -2,12 +2,18 @@ import 'dart:io';
 
 import 'package:favorite_places/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   //tạo hàm tạo không nhận tham số: danh sách trình khởi tạo
   UserPlacesNotifier() : super([]);
-  void addPlace(String title, File image,PlaceLocation location) {
-    final newPlace = Place(title: title, image: image,location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final addDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${addDir.path}/$filename');
+
+    final newPlace = Place(title: title, image: copiedImage, location: location);
     //tạo danh sách trạng thái mới và thêm new Place vào vào danh sách khởi tạo ban đầu
     state = [...state, newPlace];
   }
