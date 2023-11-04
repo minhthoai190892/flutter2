0919cbdb201cde358df34a9035670e9fa031a606
minhthoai190892 <minhthoai190892@gmail.com>
2023-11-04 10:18:42 +0700

    qr-code-09-Scan-QR-Code

--- qr_code_getx/lib/app/modules/home/controllers/home_controller.dart
+++ qr_code_getx/lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
-import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:qr_code_getx/app/data/models/product_model.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
@@ -45,5 +46,33 @@ class HomeController extends GetxController {
     final file = File('${dir.path}/example.pdf');
     await file.writeAsBytes(await pdf.save());
     await OpenFile.open(file.path);
+    print(file);
+  }
+
+  Future<Map<String, dynamic>> getProductById(String codeBarang) async {
+    try {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      var hasil = await firestore
+          .collection('products')
+          .where('code', isEqualTo: codeBarang)
+          .get();
+      if (hasil.docs.first.data().isEmpty) {
+        return {
+          'error': false,
+          'message': "faild",
+        };
+      }
+      Map<String, dynamic> data = hasil.docs.first.data();
+      return {
+        'error': false,
+        'message': "success",
+        'data': ProductModel.fromJson(data)
+      };
+    } catch (e) {
+      return {
+        'error': false,
+        'message': "faild",
+      };
+    }
   }
 }
