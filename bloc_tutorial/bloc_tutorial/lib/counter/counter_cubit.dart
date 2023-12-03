f85b14e28829c44e140326eddd64ef13b0bd95fa
minhthoai <minhthoai190892@gmail.com>
2023-12-03 22:54:00 +0700

    bloc_counter

--- bloc_tutorial/bloc_tutorial/lib/counter/counter_cubit.dart
+++ bloc_tutorial/bloc_tutorial/lib/counter/counter_cubit.dart
@@ -6,8 +6,13 @@ part 'counter_state.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(10);
   void increment() {
-    print('before: $state');
     emit(state + 1);
-    print('after: $state');
+  }
+
+  void decrement() {
+    if (state==0) {
+      return;
+    }
+    emit(state - 1);
   }
 }
--- bloc_tutorial/bloc_tutorial/lib/main.dart
+++ bloc_tutorial/bloc_tutorial/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bloc_tutorial/counter/counter_cubit.dart';
+import 'package:bloc_tutorial/inc_dec_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -31,11 +36,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final counterCubit = CounterCubit();
-
   @override
   Widget build(BuildContext context) {
-    final counter = counterCubit.state;
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -48,21 +51,40 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BlocBuilder<CounterCubit, int>(
+              bloc: counterCubit,
+              builder: (context, state) {
+                return Text(
+                  '$state',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-       
-          counterCubit.increment();
-          setState(() {});
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => IncDecPage(),
+                  ));
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.navigate_next),
+          ),
+          // FloatingActionButton(
+          //   onPressed: () {
+          //     counterCubit.decrement();
+          //   },
+          //   tooltip: 'Decrement',
+          //   child: const Icon(Icons.remove),
+          // ),
+        ],
       ),
     );
   }
