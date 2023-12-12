7470d909224d3d4376068d2590c664e1205f1445
minhthoai <minhthoai190892@gmail.com>
2023-12-13 01:14:26 +0700

    bloc_todo_Bloc_StreamSubscription

---
+++ bloc_tutorial/todo_cubit_listener/lib/blocs/active_todo_count/active_todo_count_state.dart
@@ -0,0 +1,41 @@
+part of 'active_todo_count_bloc.dart';
+
+class ActiveTodoCountState extends Equatable {
+  final int activeTodoCount;
+  const ActiveTodoCountState({
+    required this.activeTodoCount,
+  });
+  factory ActiveTodoCountState.initial() {
+    return const ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'count': activeTodoCount,
+    };
+  }
+
+  factory ActiveTodoCountState.fromMap(Map<String, dynamic> map) {
+    return ActiveTodoCountState(
+      activeTodoCount: map['count'] as int,
+    );
+  }
+
+  // String toJson() => json.encode(toMap());
+
+  // factory ActiveTodoCountState.fromJson(String source) => ActiveTodoCountState.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object> get props => [activeTodoCount];
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+}
