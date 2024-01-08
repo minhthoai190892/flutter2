16474610b02f4eb64c477e821d0cb7f202cb6e9c
minhthoai <minhthoai190892@gmail.com>
2024-01-08 23:32:45 +0700

    UI-VIDEO-SHARING-APP_part1-a

--- UI/S34/video_sharing_app_ui/lib/views/video_sharing_home_page.dart
+++ UI/S34/video_sharing_app_ui/lib/views/video_sharing_home_page.dart
@@ -60,15 +60,16 @@ class VideoSharingVideoHomePage extends StatelessWidget {
               const SizedBox(
                 height: 8,
               ),
-              Container(
+              SizedBox(
                 height: 100,
-                color: Colors.pinkAccent,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (context, index) => const Padding(
                     padding: EdgeInsets.all(2.0),
                     child: CircleAvatar(
                       radius: 48,
+                      backgroundImage: NetworkImage(
+                          'https://fastly.picsum.photos/id/40/4106/2806.jpg?hmac=MY3ra98ut044LaWPEKwZowgydHZ_rZZUuOHrc3mL5mI'),
                     ),
                   ),
                 ),
@@ -104,7 +105,16 @@ class VideoSharingVideoHomePage extends StatelessWidget {
                       ),
                       SizedBox(
                         height: size.height / 1.7,
-                        child: const Placeholder(),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: (context, index) => Container(
+                            width: 260,
+                            margin: const EdgeInsets.only(right: 12),
+                            padding: const EdgeInsets.all(12),
+                            decoration: const BoxDecoration(color: Colors.grey),
+                          ),
+                        ),
                       ),
                       const Text.rich(
                         TextSpan(
