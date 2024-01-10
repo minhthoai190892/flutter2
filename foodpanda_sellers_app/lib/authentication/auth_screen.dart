f6615b286988ffaf33459faf272fe35c3c71568e
minhthoai <minhthoai190892@gmail.com>
2024-01-10 15:04:02 +0700

    FOODPANDEA_SELLERS_APP-tabbar-login-register

--- foodpanda_sellers_app/lib/authentication/auth_screen.dart
+++ foodpanda_sellers_app/lib/authentication/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodpanda_sellers_app/authentication/login_screen.dart';
+import 'package:foodpanda_sellers_app/authentication/register.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -10,8 +12,81 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text('Welcome'),),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.cyan,
+                  Colors.amber,
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.mirror,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.blue,
+          title: const Text(
+            'iFood',
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.white,
+              fontFamily: 'Lobster',
+            ),
+          ),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+            indicatorColor: Colors.white,
+            // indicatorWeight: 6,
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.cyan,
+                Colors.amber,
+              ],
+            ),
+          ),
+          child: const TabBarView(
+            children: [
+              LoginScreen(),
+              RegisterScreen(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- foodpanda_sellers_app/lib/main.dart
+++ foodpanda_sellers_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodpanda_sellers_app/authentication/auth_screen.dart';
 import 'package:foodpanda_sellers_app/splash_screen/splash_screen.dart';
 
 Future<void> main() async {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MySplashScreen(),
+      home: const AuthScreen(),
     );
   }
 }
