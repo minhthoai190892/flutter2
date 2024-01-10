57a43ffef639b34756eada260bd8030a68f20f4a
minhthoai <minhthoai190892@gmail.com>
2024-01-10 14:09:25 +0700

    FOODPANDEA_SELLERS_APP-splash-screen

--- foodpanda_sellers_app/lib/main.dart
+++ foodpanda_sellers_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodpanda_sellers_app/splash_screen/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:Scaffold( ),
+      home: const MySplashScreen(),
     );
   }
 }
