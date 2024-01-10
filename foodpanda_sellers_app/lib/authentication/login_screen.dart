2939e6c4fd240946ba2ed476518173388ff5e084
minhthoai <minhthoai190892@gmail.com>
2024-01-10 21:26:01 +0700

    FOODPANDEA_SELLERS_APP-login-form

--- foodpanda_sellers_app/lib/authentication/login_screen.dart
+++ foodpanda_sellers_app/lib/authentication/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_text_field_widget.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -8,10 +10,69 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('LoginScreen'),
+    final size = MediaQuery.of(context).size;
+    print('object: $size');
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Image.asset(
+                'assets/images/seller.png',
+                height: size.height * 0.45,
+              ),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextFieldWidget(
+                  controller: emailController,
+                  hintText: 'Enail',
+                  iconData: Icons.email,
+                  isObserce: false,
+                ),
+                CustomTextFieldWidget(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  iconData: Icons.password,
+                  isObserce: true,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () => print('Login '),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+              padding: const EdgeInsets.symmetric(horizontal: 90, vertical: 10),
+            ),
+            child: const Text(
+              'Signup',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
     );
   }
 }
