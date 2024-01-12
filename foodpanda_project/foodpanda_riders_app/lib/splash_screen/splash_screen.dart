0332fd4c361a7f9bc0ffbd1ba5cff466c86d9506
minhthoai <minhthoai190892@gmail.com>
2024-01-13 00:52:09 +0700

    FOODPANDEA_RIDERS_APP-splash-screen

---
+++ foodpanda_project/foodpanda_riders_app/lib/splash_screen/splash_screen.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../global/global.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 3), () {
+      if (firebaseAuth.currentUser != null) {
+        // Navigator.push(
+        //     context,
+        //     MaterialPageRoute(
+        //       builder: (context) => const HomeScreen(),
+        //     ));
+      } else {
+        // Navigator.push(
+        //     context,
+        //     MaterialPageRoute(
+        //       builder: (context) => const AuthScreen(),
+        //     ));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(18),
+                child: Text(
+                  "World's Largest & Number Online Food App",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 40,
+                      fontFamily: 'Signatra',
+                      letterSpacing: 3),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
