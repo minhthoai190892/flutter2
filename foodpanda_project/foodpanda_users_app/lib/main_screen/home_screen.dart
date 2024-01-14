ab8c260ebd7e9ee941674c21ced115df518b7489
minhthoai <minhthoai190892@gmail.com>
2024-01-15 00:30:51 +0700

    FOODPANDEA_USERS_APP-carousel

--- foodpanda_project/foodpanda_users_app/lib/main_screen/home_screen.dart
+++ foodpanda_project/foodpanda_users_app/lib/main_screen/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:foodpanda_users_app/widgets/my_drawer.dart';
 
 import '../authentication/auth_screen.dart';
 import '../global/global.dart';
+import '../slider_data/slider_data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +14,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List sliderList = sliders;
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white),
@@ -44,6 +49,52 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       drawer: const MyDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                height: size.height * 0.3,
+                width: size.width,
+                child: CarouselSlider(
+                    items: sliderList
+                        .map(
+                          (index) => Container(
+                            decoration:
+                                const BoxDecoration(color: Colors.black),
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Image.asset(
+                                index,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    options: CarouselOptions(
+                      height: size.height * 0.3,
+                      aspectRatio: 16 / 9,
+                      viewportFraction: 0.8,
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      reverse: false,
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 3),
+                      autoPlayAnimationDuration:
+                          const Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.decelerate,
+                      enlargeCenterPage: true,
+                      enlargeFactor: 0.3,
+                      // onPageChanged: callbackFunction,
+                      scrollDirection: Axis.horizontal,
+                    )),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
