519e82c3a70cfa2695db51eed0d3055c20272585
minhthoai <minhthoai190892@gmail.com>
2024-01-16 13:52:32 +0700

    FOODPANDEA_SELLERS_APP-get-image-by-camera-or-gellery

--- foodpanda_project/foodpanda_sellers_app/lib/upload_screens/munus_upload_screens.dart
+++ foodpanda_project/foodpanda_sellers_app/lib/upload_screens/munus_upload_screens.dart
@@ -1,7 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class MenusUploadScreen extends StatelessWidget {
+class MenusUploadScreen extends StatefulWidget {
   const MenusUploadScreen({super.key});
+
+  @override
+  State<MenusUploadScreen> createState() => _MenusUploadScreenState();
+}
+
+class _MenusUploadScreenState extends State<MenusUploadScreen> {
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
+  takeImage(BuildContext  mContent) {
+    return showDialog(
+      context: mContent,
+      builder: (context) => SimpleDialog(
+        title: const Text(
+          'Menu Image',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.amber),
+        ),
+        children: [
+          SimpleDialogOption(
+            onPressed: () {
+              captureImageWithCamera(ImageSource.camera);
+            },
+            child: const Text(
+              'Capture with Camera',
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () {
+                captureImageWithCamera(ImageSource.gallery);
+            },
+            child: const Text(
+              'Capture with Camera',
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          SimpleDialogOption(
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Colors.grey),
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  captureImageWithCamera(ImageSource source) async {
+    Navigator.pop(context);
+    // lấy hình ảnh
+    imageXFile = await _picker.pickImage(
+      source: source,
+      maxHeight: 720,
+      maxWidth: 1280,
+    );
+    setState(() {
+      imageXFile;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,8 +120,11 @@ class MenusUploadScreen extends StatelessWidget {
               ),
               ElevatedButton(
                 style: const ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.amber)),
-                onPressed: () {},
+                  backgroundColor: MaterialStatePropertyAll(Colors.amber),
+                ),
+                onPressed: () {
+                  takeImage(context);
+                },
                 child: const Text(
                   'Add New Menu',
                   style: TextStyle(
