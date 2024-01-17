d4c5892ea9a3e895fd9ee7d623cc5b28905d371c
minhthoai <minhthoai190892@gmail.com>
2024-01-17 12:04:40 +0700

    FOODPANDEA_SELLERS_APP-show-menus

--- foodpanda_project/foodpanda_sellers_app/lib/main_screen/home_screen.dart
+++ foodpanda_project/foodpanda_sellers_app/lib/main_screen/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:foodpanda_sellers_app/global/global.dart';
+import 'package:foodpanda_sellers_app/model/menus_model.dart';
 import 'package:foodpanda_sellers_app/upload_screens/munus_upload_screens.dart';
+import 'package:foodpanda_sellers_app/widgets/info_design_widget.dart';
 import 'package:foodpanda_sellers_app/widgets/my_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -57,6 +60,43 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       drawer: const MyDrawerWidget(),
+      body: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: ListTile(
+              title: Text('My Menu'),
+            ),
+          ),
+          StreamBuilder(
+            stream: firebaseFirestore
+                .collection('sellers')
+                .doc(sharedPreferences!.getString('uid'))
+                .collection('menus')
+                .snapshots(),
+            builder: (context, snapshot) {
+              return !snapshot.hasData
+                  ? const SliverToBoxAdapter(
+                      child: CircularProgressIndicator(),
+                    )
+                  : SliverStaggeredGrid.countBuilder(
+                      crossAxisCount: 1,
+                      staggeredTileBuilder: (index) =>
+                          const StaggeredTile.fit(1),
+                      itemBuilder: (context, index) {
+                        Menus model = Menus.fromMap(
+                          snapshot.data!.docs[index].data(),
+                        );
+                        return InfoDesignWidget(
+                          model: model,
+                          context: context,
+                        );
+                      },
+                      itemCount: snapshot.data!.docs.length,
+                    );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
