9d23816835b9785417c7b2bc9a14464c24e27e54
minhthoai <minhthoai190892@gmail.com>
2024-01-18 13:10:23 +0700

    FOODPANDEA_USERS_APP-show-item-detail-screen

--- foodpanda_project/foodpanda_users_app/lib/main_screen/item_detail_screen.dart
+++ foodpanda_project/foodpanda_users_app/lib/main_screen/item_detail_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:foodpanda_users_app/models/items_model.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 
 import '../widgets/my_app_bar.dart';
 
@@ -16,8 +17,85 @@ class ItemDetailScreen extends StatefulWidget {
 }
 
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
+  final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return MyAppBar(widget: widget);
+    return Scaffold(
+      appBar: const MyAppBarWidget(),
+      // drawer: const MyDrawerWidget(),
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(widget.model.thumbnailUrl),
+              NumberInputPrefabbed.roundedButtons(
+                controller: controller,
+                incDecBgColor: Colors.amber,
+                min: 1,
+                max: 9,
+                initialValue: 1,
+                buttonArrangement: ButtonArrangement.incLeftDecRight,
+              ),
+              Text(
+                widget.model.title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                widget.model.longDescription,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              Text(
+                '\$ ${widget.model.price}',
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              InkWell(
+                onTap: () {
+                  // add to cart
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.cyan,
+                        Colors.amber,
+                      ],
+                      begin: FractionalOffset(0.0, 0.0),
+                      end: FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.mirror,
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Add to Cart',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
