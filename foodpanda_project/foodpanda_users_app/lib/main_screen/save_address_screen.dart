24b3686a3cf79d9d80eef64580fea52495b2f244
minhthoai <minhthoai190892@gmail.com>
2024-01-23 02:01:17 +0700

    FOODPANDEA_USERS_APP-get-location-user

--- foodpanda_project/foodpanda_users_app/lib/main_screen/save_address_screen.dart
+++ foodpanda_project/foodpanda_users_app/lib/main_screen/save_address_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foodpanda_users_app/widgets/text_field_widget.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../widgets/simple_app_bar_widget.dart';
 
@@ -13,8 +15,66 @@ class SaveAddressScreen extends StatelessWidget {
   final _completeAddressController = TextEditingController();
   final _locationController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  late Position position;
+  late List<Placemark> placemarks;
+  late String completeAddress;
+  _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    Position newPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(newPosition.latitude);
+    print(newPosition.longitude);
+    position = newPosition;
+    placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark pMark = placemarks[0];
+    completeAddress =
+        '${pMark.subThoroughfare}, ${pMark.thoroughfare},${pMark.subLocality} ${pMark.locality}, ${pMark.subAdministrativeArea}, ${pMark.administrativeArea}  ${pMark.postalCode}, ${pMark.country}';
+    _locationController.text = completeAddress;
+    _nameController.text =
+        '${pMark.subThoroughfare}, ${pMark.thoroughfare},${pMark.subLocality}';
+    _cityController.text =
+        '${pMark.subAdministrativeArea}, ${pMark.administrativeArea} ${pMark.postalCode}';
+    _statController.text = '${pMark.country}';
+    _completeAddressController.text = completeAddress;
+    print('Location: ${_locationController.text}');
+  }
+
   @override
   Widget build(BuildContext context) {
+    // _determinePosition();
     return Scaffold(
       appBar: const SimpleAppBar(),
       body: SingleChildScrollView(
@@ -57,6 +117,7 @@ class SaveAddressScreen extends StatelessWidget {
               style: ElevatedButton.styleFrom(backgroundColor: Colors.cyan),
               onPressed: () {
                 // get current location with address
+                _determinePosition();
               },
               icon: const Icon(
                 Icons.location_on,
