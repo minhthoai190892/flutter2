f67dc2211ce6076a21e11ca58768f3bab30ff81e
minhthoai <minhthoai190892@gmail.com>
2024-02-06 23:03:55 +0700

    CHATTY_SharedPreferences-login_page

--- chatty/lib/app/modules/sign_in/views/sign_in_view.dart
+++ chatty/lib/app/modules/sign_in/views/sign_in_view.dart
@@ -1,4 +1,6 @@
+import 'package:chatty/app/common/style/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -6,17 +8,75 @@ import '../controllers/sign_in_controller.dart';
 
 class SignInView extends GetView<SignInController> {
   const SignInView({Key? key}) : super(key: key);
+  Widget _buildLogo() => Container(
+        margin: EdgeInsets.only(top: 100.h, bottom: 80.h),
+        child: Text(
+          'Chatty .',
+          style: TextStyle(
+              color: AppColor.primaryText,
+              fontWeight: FontWeight.bold,
+              fontSize: 34.sp),
+        ),
+      );
+  Widget _buildThirdPartyGoogleLogin({required String loginType}) => Container(
+        width: 295.w,
+        height: 44.h,
+        margin: EdgeInsets.only(bottom: 15.h),
+        padding: EdgeInsetsDirectional.all(10.h),
+        decoration: BoxDecoration(
+          color: AppColor.primaryBackground,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(
+                0,
+                1,
+              ),
+            ),
+          ],
+          borderRadius: const BorderRadius.all(
+            Radius.circular(
+              5,
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 40.w, right: 30.w),
+              child: Image.asset('assets/icons/google.png'),
+            ),
+            Container(
+              child: Text(
+                loginType,
+                style: TextStyle(
+                  color: AppColor.primaryText,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 14.sp,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.primarySecondaryBackground,
       appBar: AppBar(
         title: const Text('SignInView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'SignInView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Column(
+          children: [
+            _buildLogo(),
+            _buildThirdPartyGoogleLogin(loginType: 'Sign in with Google'),
+            _buildThirdPartyGoogleLogin(loginType: 'Google Login'),
+            _buildThirdPartyGoogleLogin(loginType: 'Google Login'),
+          ],
         ),
       ),
     );
