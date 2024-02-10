9ba058d07c56ccb17b62c47c4cca22c0c88cf119
minhthoai <minhthoai190892@gmail.com>
2024-02-11 01:45:12 +0700

    CHATTY_SharedPreferences-login_page-phone-number-widget

--- chatty/lib/app/modules/sign_in/views/sign_in_view.dart
+++ chatty/lib/app/modules/sign_in/views/sign_in_view.dart
@@ -23,71 +23,129 @@ class SignInView extends GetView<SignInController> {
 
   /// login type
   Widget _buildThirdPartyGoogleLogin(
-          {required String loginType, required String iconType}) =>
-      Container(
-        width: 295.w,
-        height: 44.h,
-        margin: EdgeInsets.only(bottom: 15.h),
-        padding: EdgeInsetsDirectional.all(10.h),
-        decoration: BoxDecoration(
-          color: AppColor.primaryBackground,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.1),
-              spreadRadius: 1,
-              blurRadius: 2,
-              offset: const Offset(
-                0,
-                1,
-              ),
-            ),
-          ],
-          borderRadius: const BorderRadius.all(
-            Radius.circular(
-              5,
+      {required String loginType,
+      required String iconType,
+      required BuildContext context}) {
+    print(MediaQuery.of(context).size);
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.06,
+      margin: EdgeInsets.only(bottom: 15.h),
+      padding: EdgeInsetsDirectional.all(10.h),
+      decoration: BoxDecoration(
+        color: AppColor.primaryBackground,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: const Offset(
+              0,
+              1,
             ),
           ),
+        ],
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            5,
+          ),
         ),
-        child: Row(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 40.w, right: 30.w),
-              child: Image.asset('assets/icons/$iconType.png'),
+      ),
+      child: Row(
+        mainAxisAlignment:
+            iconType == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
+        children: [
+          iconType == ''
+              ? Container()
+              : Container(
+                  padding: EdgeInsets.only(left: 40.w, right: 30.w),
+                  child: Image.asset('assets/icons/$iconType.png'),
+                ),
+          Text(
+            'Sign in with $loginType',
+            style: TextStyle(
+              color: AppColor.primaryText,
+              fontWeight: FontWeight.normal,
+              fontSize: 14.sp,
             ),
-            Text(
-              'Sign in with $loginType',
-              style: TextStyle(
-                color: AppColor.primaryText,
-                fontWeight: FontWeight.normal,
-                fontSize: 14.sp,
-              ),
-            ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.primarySecondaryBackground,
-      appBar: AppBar(
-        title: const Text('SignInView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            _buildLogo(),
-            _buildThirdPartyGoogleLogin(
-                loginType: 'Sign in with Google', iconType: 'google'),
-            _buildThirdPartyGoogleLogin(
-                loginType: 'Sign in with Google', iconType: 'facebook'),
-            _buildThirdPartyGoogleLogin(
-                loginType: 'Sign in with Apple', iconType: 'apple'),
-            buildOrWidget()
-          ],
+      // appBar: AppBar(
+      //   title: const Text('SignInView'),
+      //   centerTitle: true,
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            children: [
+              _buildLogo(),
+              _buildThirdPartyGoogleLogin(
+                loginType: 'Sign in with Google',
+                iconType: 'google',
+                context: context,
+              ),
+              _buildThirdPartyGoogleLogin(
+                loginType: 'Sign in with Google',
+                iconType: 'facebook',
+                context: context,
+              ),
+              _buildThirdPartyGoogleLogin(
+                loginType: 'Sign in with Apple',
+                iconType: 'apple',
+                context: context,
+              ),
+              buildOrWidget(),
+              _buildThirdPartyGoogleLogin(
+                loginType: 'Phone number',
+                iconType: '',
+                context: context,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              buildSignupWidget(),
+            
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Column buildSignupWidget() {
+    return Column(
+              children: [
+                  Text(
+              'Already have an account',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColor.primaryText,
+                fontWeight: FontWeight.normal,
+                fontSize: 12.sp,
+              ),
+            ),
+            GestureDetector(
+              child: Text(
+                'Sign up here',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColor.primaryElement,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 12.sp,
+                ),
+              ),
+            ),
+              ],
+            );
   }
 
   Container buildOrWidget() {
