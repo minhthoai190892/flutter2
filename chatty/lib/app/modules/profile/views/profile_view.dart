9434cea42430a020e59125f8904986d19eacb9ff
minhthoai <minhthoai190892@gmail.com>
2024-02-14 17:54:51 +0700

    CHATTY_PROFILE-photo

--- chatty/lib/app/modules/profile/views/profile_view.dart
+++ chatty/lib/app/modules/profile/views/profile_view.dart
@@ -1,4 +1,6 @@
+import 'package:chatty/app/common/style/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -6,17 +8,51 @@ import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
   const ProfileView({Key? key}) : super(key: key);
+  Widget _buildProfilePhoto({required Size size}) => Stack(
+        children: [
+          Container(
+            width: size.width / 3.2.w,
+            height: size.width / 3.2.w,
+            decoration: BoxDecoration(
+              color: AppColor.primarySecondaryBackground,
+              borderRadius: BorderRadius.all(
+                Radius.circular(60.w),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 2,
+                  spreadRadius: 1,
+                  color: Colors.grey.withOpacity(0.1),
+                  offset: const Offset(0, 1),
+                ),
+              ],
+            ),
+            child: Image.asset('assets/images/account_header.png'),
+          ),
+        ],
+      );
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    print(size);
     return Scaffold(
       appBar: AppBar(
         title: const Text('ProfileView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                child: Column(
+                  children: [
+                    _buildProfilePhoto(size: size),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
