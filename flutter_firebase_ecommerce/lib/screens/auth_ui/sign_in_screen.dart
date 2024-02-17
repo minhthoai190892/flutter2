c8dd849d8d79baec43ac9cbbdfb2787c02a87aa5
minhthoai <minhthoai190892@gmail.com>
2024-02-17 23:05:39 +0700

    flutter_firebase_ecommerce_Placing-an-order-in-flutter-Part-2-

--- flutter_firebase_ecommerce/lib/screens/auth_ui/sign_in_screen.dart
+++ flutter_firebase_ecommerce/lib/screens/auth_ui/sign_in_screen.dart
@@ -41,152 +41,156 @@ class _SignInScreenState extends State<SignInScreen> {
                 fontWeight: FontWeight.bold, color: AppConstant.appTextColor),
           ),
         ),
-        body: Column(
-          children: [
-            isKeyboardVisible
-                ? const Text(
-                    'Welcome to app',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  )
-                : Column(
-                    children: [
-                      Lottie.asset('assets/images/splash-icon.json'),
-                    ],
-                  ),
-            SizedBox(
-              height: Get.height / 30,
-            ),
-            // email
-            TextFormFieldWidget(
-                controller: userEmail,
-                hitText: 'Email',
-                keyboardType: TextInputType.emailAddress,
-                prefixIcon: Icons.email),
-            // passwod
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Obx(() => TextFormField(
-                      controller: userPassword,
-                      cursorColor: AppConstant.appSecondoryColor,
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: signInController.isPasswordVisible.value,
-                      decoration: InputDecoration(
-                          hintText: 'Password',
-                          contentPadding:
-                              const EdgeInsets.only(top: 2, left: 8),
-                          prefixIcon: const Icon(Icons.password),
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              signInController.isPasswordVisible.toggle();
-                            },
-                            child: signInController.isPasswordVisible.value
-                                ? const Icon(Icons.visibility)
-                                : const Icon(Icons.visibility_off),
-                          ),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 12),
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: () => Get.to(() => const ForgetPasswordScreen()),
-                child: const Text(
-                  'Forget Password?',
-                  style: TextStyle(
-                      color: AppConstant.appSecondoryColor,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: Get.height / 20,
-            ),
-            Material(
-              child: Container(
-                width: Get.width / 2,
-                height: Get.height / 18,
-                decoration: BoxDecoration(
-                    color: AppConstant.appMainColor,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Center(
-                  child: TextButton(
-                    onPressed: () async {
-                      String email = userEmail.text.trim();
-                      String password = userPassword.text.trim();
-                      if (email.isEmpty || password.isEmpty) {
-                        Get.snackbar('Error', 'Please enter all details');
-                      } else {
-                        UserCredential? userCredential = await signInController
-                            .signInMethod(email, password);
-                        var userData = await getUserDataController
-                            .getUserData(userCredential!.user!.uid);
-                        if (userCredential != null) {
-                          if (userCredential.user!.emailVerified) {
-                            if (userData[0]['isAdmin'] == true) {
-                              Get.snackbar(
-                                  'Success Admin Login', 'Login successfully',
-                                  snackPosition: SnackPosition.BOTTOM,
-                                  backgroundColor: AppConstant.appMainColor,
-                                  colorText: AppConstant.appTextColor);
-                              Get.offAll(() => const AdminMainScrren());
-                            } else {
-                               Get.snackbar(
-                                  'Success User Login', 'Login successfully',
-                                  snackPosition: SnackPosition.BOTTOM,
-                                  backgroundColor: AppConstant.appMainColor,
-                                  colorText: AppConstant.appTextColor);
-                              Get.offAll(() => const MainScreen());
-                            }
-                          } else {
-                            Get.snackbar('Error',
-                                'Please verify your email before logging',
-                                snackPosition: SnackPosition.BOTTOM,
-                                backgroundColor: AppConstant.appMainColor,
-                                colorText: AppConstant.appTextColor);
-                          }
-                        } else {
-                          Get.snackbar('Error', 'Please try again',
-                              snackPosition: SnackPosition.BOTTOM,
-                              backgroundColor: AppConstant.appMainColor,
-                              colorText: AppConstant.appTextColor);
-                        }
-                      }
-                    },
-                    child: const Text(
-                      'SIGN IN',
-                      style: TextStyle(color: AppConstant.appTextColor),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              isKeyboardVisible
+                  ? const Text(
+                      'Welcome to app',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )
+                  : Column(
+                      children: [
+                        Lottie.asset('assets/images/splash-icon.json'),
+                      ],
                     ),
-                  ),
+              SizedBox(
+                height: Get.height / 30,
+              ),
+              // email
+              TextFormFieldWidget(
+                  controller: userEmail,
+                  hitText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                  prefixIcon: Icons.email),
+              // passwod
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Obx(() => TextFormField(
+                        controller: userPassword,
+                        cursorColor: AppConstant.appSecondoryColor,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: signInController.isPasswordVisible.value,
+                        decoration: InputDecoration(
+                            hintText: 'Password',
+                            contentPadding:
+                                const EdgeInsets.only(top: 2, left: 8),
+                            prefixIcon: const Icon(Icons.password),
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                signInController.isPasswordVisible.toggle();
+                              },
+                              child: signInController.isPasswordVisible.value
+                                  ? const Icon(Icons.visibility)
+                                  : const Icon(Icons.visibility_off),
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      )),
                 ),
               ),
-            ),
-            SizedBox(
-              height: Get.height / 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Don't have an account? ",
-                  style: TextStyle(color: AppConstant.appSecondoryColor),
-                ),
-                GestureDetector(
-                  onTap: () => Get.offAll(() => const SignUpScreen()),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 12),
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () => Get.to(() => const ForgetPasswordScreen()),
                   child: const Text(
-                    "Sign up",
+                    'Forget Password?',
                     style: TextStyle(
                         color: AppConstant.appSecondoryColor,
                         fontWeight: FontWeight.bold),
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+              SizedBox(
+                height: Get.height / 20,
+              ),
+              Material(
+                child: Container(
+                  width: Get.width / 2,
+                  height: Get.height / 18,
+                  decoration: BoxDecoration(
+                      color: AppConstant.appMainColor,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        String email = userEmail.text.trim();
+                        String password = userPassword.text.trim();
+                        if (email.isEmpty || password.isEmpty) {
+                          Get.snackbar('Error', 'Please enter all details');
+                        } else {
+                          UserCredential? userCredential =
+                              await signInController.signInMethod(
+                                  email, password);
+                          var userData = await getUserDataController
+                              .getUserData(userCredential!.user!.uid);
+                          if (userCredential != null) {
+                            if (userCredential.user!.emailVerified) {
+                              if (userData[0]['isAdmin'] == true) {
+                                Get.snackbar(
+                                    'Success Admin Login', 'Login successfully',
+                                    snackPosition: SnackPosition.BOTTOM,
+                                    backgroundColor: AppConstant.appMainColor,
+                                    colorText: AppConstant.appTextColor);
+                                Get.offAll(() => const AdminMainScrren());
+                              } else {
+                                Get.snackbar(
+                                    'Success User Login', 'Login successfully',
+                                    snackPosition: SnackPosition.BOTTOM,
+                                    backgroundColor: AppConstant.appMainColor,
+                                    colorText: AppConstant.appTextColor);
+                                Get.offAll(() => const MainScreen());
+                              }
+                            } else {
+                              Get.snackbar('Error',
+                                  'Please verify your email before logging',
+                                  snackPosition: SnackPosition.BOTTOM,
+                                  backgroundColor: AppConstant.appMainColor,
+                                  colorText: AppConstant.appTextColor);
+                            }
+                          } else {
+                            Get.snackbar('Error', 'Please try again',
+                                snackPosition: SnackPosition.BOTTOM,
+                                backgroundColor: AppConstant.appMainColor,
+                                colorText: AppConstant.appTextColor);
+                          }
+                        }
+                      },
+                      child: const Text(
+                        'SIGN IN',
+                        style: TextStyle(color: AppConstant.appTextColor),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: Get.height / 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account? ",
+                    style: TextStyle(color: AppConstant.appSecondoryColor),
+                  ),
+                  GestureDetector(
+                    onTap: () => Get.offAll(() => const SignUpScreen()),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                          color: AppConstant.appSecondoryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
